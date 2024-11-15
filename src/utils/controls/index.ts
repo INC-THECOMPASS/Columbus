2437f880a49e8186cc862d713ee6ff5f25f06fc6
jeongwoo903 <www8565@naver.com>
2024-11-15 23:23:41 +0900

    fix: 기능 적용 범위 수정

--- src/utils/controls/index.ts
+++ src/utils/controls/index.ts
@@ -1,4 +1,6 @@
-// formats barrel file
+// controls barrel file
+export * from './common/common';
+export * from './preventCopy';
 export * from './preventContextMenu';
 export * from './preventDrag';
 export * from './preventKeydown';
